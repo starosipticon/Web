8789d4ed84f6067e1e27e307c3621824bd7cccec
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-23 20:35:11 +0300

    Update n3.js

--- n3.js
+++ n3.js
@@ -1,31 +1,10 @@
-let time = prompt();
-let goToStore = false;
-let goToMarket = false;
-let goToDairy = false;
-
-if (time >= 7 && time < 20)
-  {
-    if(time >= 8 && time < 19)
-      {
-        if (!(time >= 13 && time < 14))
-          {goToDairy = true;}
-        else{ goToStore = true;}
-      }
-    else{
-      if (time >= 14 && time <15)
-        {
-         goToMarket = true;
-        }
-      else{
-        if(time >= 9 && time < 17){
-        goToStore = true;}
-        else{goToMarket = true;}
-      }
-    }
-  }
-if(goToStore)
-  {alert("В магазин");}
-if(goToMarket)
-  {alert("На рынок");}
-if (goToDairy)
-  {alert("На фабрику");}
+let calculateSalary = function (blackSalary) {
+let percent = 0.35
+if (blackSalary >= 100000) {
+percent = 0.45;
+}
+let tax = percent * blackSalary;
+let whiteSalary = blackSalary - tax;
+return whiteSalary;
+};
+console.log(calculateSalary(100000))
