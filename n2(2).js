e5cc73579e09afb05d19a05f166d5929158bd313
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-24 00:37:53 +0300

    Update n2(2).js

--- n2(2).js
+++ n2(2).js
@@ -1,12 +1,31 @@
-let numbers = [3, 5, 15, 6, 2, 1];
+let numbers = [6, 2, 13, 9, 7, 3];
+let lastIndex = -1;
+let numWeSearch = 6;
 let swap;
 
-for (let i = 0; i < numbers.length; i++) {
-for (let j = i + 1; j < numbers.length; j++) {
-if (numbers[j] < numbers[i]) {
-swap = numbers[i];
-numbers[i] = numbers[j];
-numbers[j] = swap;}
+for (let i = 0; i < numbers.length; i++) 
+{
+  for (let j = i + 1; j < numbers.length; j++) 
+   {
+    if (numbers[j] < numbers[i]) 
+    {
+      swap = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = swap;
+    }
+  }
 }
+
+for (let i = 0; i <= numbers.length - 1; i++) 
+{
+ if (numbers[i] == numWeSearch)
+ {
+   lastIndex = i;
+ }
 }
-console.log(numbers);
+
+if (lastIndex == -1)
+{
+  console.log('элемент не найден');
+}
+else console.log(lastIndex);
