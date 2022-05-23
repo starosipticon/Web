ecff29a92b5296fa49eb50ab556fa41f23ce0e68
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-24 00:39:39 +0300

    Update n5.js

--- n5.js
+++ n5.js
@@ -1,27 +1,20 @@
-let getSortedArray = function (mass, key) {
-for (let i = 0; i < mass.length - 1; i++) {
-for (let j = i + 1; j < mass.length; j++) {
-if (mass[j][key] <= mass[i][key]) {
-swap = mass[i];
-mass[i] = mass[j];
-mass[j] = swap;
+var getSortedArray = function (mass, key) 
+{
+  for (let i = 0; i < mass.length - 1; i++) 
+  {
+    for (let j = i + 1; j < mass.length; j++) 
+    {
+      if (mass[j][key] <= mass[i][key]) 
+      {
+        swap = mass[i];
+        mass[i] = mass[j];
+        mass[j] = swap;
+      }
+    }
+  }
+  return mass;
 }
-}
-}
-return mass;
-}
-let n=[
- {
- name: 'Петя',
- age: 5
- },
- {
- name: 'Лёля',
- age: 2
- },
- {
- name: 'Сима',
- age: 3
- }
-];
+
+let n=[{name: 'Петя', age: 5}, {name: 'Лёля', age: 2}, {name: 'Сима', age: 3}];
+       
 console.log(getSortedArray(n,'age'));
