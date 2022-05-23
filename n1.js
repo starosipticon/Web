e4a6ac459fc93b03f5e7a734b3ed8ec5e2998448
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-23 20:33:52 +0300

    Create n1.js

---
+++ n1.js
@@ -0,0 +1,6 @@
+let groceries = ['чай', 'шпроты', 'печенье', 'сахар', 'чипсы'];
+let shoppingList = '';
+for (let i = 0; i <= groceries.length - 1; i++) {
+ shoppingList=shoppingList+groceries[i]+", "
+}
+console.log(shoppingList);
