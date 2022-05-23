1c2868ef8d9f03c32a05c18a59a0e79da3a61794
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-23 20:03:56 +0300

    Create n2(2).js

---
+++ n2(2).js
@@ -0,0 +1,33 @@
+let isRaining = false;
+let temperature = 25;
+let minutes;
+if (isRaining)
+  {
+    minutes = 0;
+  }
+else{
+  if (temperature > 35 && temperature < 0)
+    {
+      minutes = 0;
+    }
+  if (temperature == 20)
+    {
+      minutes = 20;
+    }
+  else
+    {
+      if (temperature > 20)
+        {
+          minutes = 20 - (temperature - 20);
+        }
+      if(temperature < 20)
+        {
+          minutes = 20 - (20-temperature);
+        }
+    }
+  if (minutes<0)
+  {
+    minutes=0
+  }
+}
+alert(minutes);
