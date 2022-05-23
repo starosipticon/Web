ec6a4e871203d3430a5aee9d767f0e0f107e26a2
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-23 20:34:39 +0300

    Update n2(1).js

--- n2(1).js
+++ n2(1).js
@@ -1,26 +1,9 @@
-let isRaining = false;
-let temperature = 25;
-let minutes;
-if (isRaining)
-  {
-    minutes = 0;
-  }
-else{
-  if (temperature >= 10 && temperature < 15)
-    {
-      minutes = 30;
-    }
-  if (temperature >= 15 && temperature < 25)
-    {
-      minutes = 40;
-    }
-  if (temperature >= 25 && temperature <= 35)
-    {
-      minutes = 20;
-    }
-  if(temperature < 10 && temperature > 35)
-    {
-      minutes  = 0;
-    }
+let numbers = [1, 2, 4 ,2, 1, 4, 3];
+let lastIndex = -1;
+let numWeSearch=2;
+for (let i = 0; i <= numbers.length - 1; i++) {
+ if (numbers[i]==numWeSearch){
+   lastIndex=i;
+ }
 }
-console.log(minutes);
+alert(lastIndex)
