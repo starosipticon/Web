1a77af1ef812dcda9ea9d6e7378ce3f58fe3a39c
starosipticon <92525208+starosipticon@users.noreply.github.com>
2022-05-24 13:18:49 +0300

    Update n4.js

--- n4.js
+++ n4.js
@@ -1,10 +1,13 @@
-let getPrice = function(time, isQuickly) {
-  let rate = 1500;
-  if (isQuickly) {
+let getPrice = function(time, isQuickly)
+{
+  cosnt rate = 1500;
+  if (isQuickly) 
+  {
     time /= 2;
     rate *= 2.5;
   }
-  if (time > 150) {
+  if (time > 150) 
+  {
     rate -= 250;
   }
   return time * rate;
